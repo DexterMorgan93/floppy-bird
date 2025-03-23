e64276754de3054ab8abc7660fd4f12b7ff0d08e
DexterMorgan93 <ogremagizk4@gmail.com>
2025-03-23 18:54:42 +0500

    init

---
+++ src/game.ts
@@ -0,0 +1,11 @@
+import { Application } from "pixi.js";
+
+export class Game {
+  private app: Application;
+
+  constructor(app: Application) {
+    this.app = app;
+  }
+
+  handleUpdate() {}
+}
---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import * as Pixi from "pixi.js";
+import "./style.css";
+import { initDevtools } from "@pixi/devtools";
+import { Game } from "./game";
+
+const app = new Pixi.Application();
+
+async function setup() {
+  await app.init({ width: 1024, height: 768, backgroundColor: 0x000000 });
+  document.body.appendChild(app.canvas);
+}
+
+(async () => {
+  await setup();
+
+  const game = new Game(app);
+  app.ticker.add(game.handleUpdate, game);
+
+  // document.addEventListener("keydown", (e) => {
+  //   game.keyboardProcessor.onKeyDown(e.key);
+  // });
+  // document.addEventListener("keyup", (e) => {
+  //   game.keyboardProcessor.onKeyUp(e.key);
+  // });
+})();
+
+initDevtools({
+  app,
+});
